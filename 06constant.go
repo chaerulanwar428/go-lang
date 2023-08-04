cc44bcfd12f81d129c49b2addcc66168781410ac
chaerulanwar <chaerulanwar428@gmail.com>
2023-08-04 21:37:50 +0700

    belajar constant

---
+++ 06constant.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	const middleName ="budi"
+	const price =1000
+
+	const (
+		firstName ="chaerul"
+		lastName = "anwar"
+	)
+
+	fmt.Println(middleName)
+	fmt.Println(price)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
