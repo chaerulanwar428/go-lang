95cba83830781cb6845c0a8cdba5f9920a0f58ae
chaerulanwar <chaerulanwar428@gmail.com>
2023-08-04 15:56:16 +0700

    belajar tipe string

---
+++ 04string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("anwar")
+	fmt.Println("chaerul anwar")
+	fmt.Println("ahmad chaerul anwar")
+
+	fmt.Println(len("anwar"))
+	fmt.Println("chaerul anwar"[0])
+	fmt.Println("ahmad chaerul anwar"[1])
+}
