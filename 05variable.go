a6373bf237132eaec267b237b0cad81ba0685cc4
chaerulanwar <chaerulanwar428@gmail.com>
2023-08-04 21:20:16 +0700

    belajar variable

---
+++ 05variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "chaerul anwar"
+	fmt.Println(name)
+
+	name = "ahmad anwar"
+	fmt.Println(name)
+	
+	var age = "budi"
+	fmt.Println(age)
+
+	country := "indonesia"
+	fmt.Println(country)
+
+	country = "america"
+	fmt.Println(country)
+
+	var (
+		firstName = "chaerul"
+		lastName = "anwar"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
